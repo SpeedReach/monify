384262850814c266b31cb9f35929c4199c7a6f8d
SpeedReach <37238439+SpeedReach@users.noreply.github.com>
2024-05-27 17:51:04 +0800

    remove sqlite3 for build

--- internal/utils/errors.go
+++ internal/utils/errors.go
@@ -2,15 +2,11 @@ package utils
 
 import (
 	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/mattn/go-sqlite3"
 )
 
 func IsDuplicateKeyError(err error) bool {
 	if pgErr, ok := err.(*pgconn.PgError); ok {
 		return pgErr.Code == "23505"
-	}
-	if sqlite, ok := err.(*sqlite3.Error); ok {
-		return sqlite.ExtendedCode == sqlite3.ErrConstraintUnique
 	}
 	return false
 }
