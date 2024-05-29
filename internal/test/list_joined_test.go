b4118d874f9edc569b1016c8aea6ad7ccb28d656
SpeedReach <37238439+SpeedReach@users.noreply.github.com>
2024-05-29 14:12:28 +0800

    fix list joined test

--- internal/test/list_joined_test.go
+++ internal/test/list_joined_test.go
@@ -8,15 +8,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListJoined(t *testing.T) {
+func TestListJoinedWithMultiGroup(t *testing.T) {
 	client := GetTestClient(t)
-	_ = client.CreateTestUser()
+	user1 := client.CreateTestUser()
+	createdGroups := map[string]string{}
 	group1, err := client.CreateGroup(context.TODO(), &monify.CreateGroupRequest{Name: "test1"})
+	assert.NoError(t, err)
+	createdGroups["test1"] = group1.GroupId
 	group2, err := client.CreateGroup(context.TODO(), &monify.CreateGroupRequest{Name: "test2"})
+	createdGroups["test2"] = group2.GroupId
+
+	_ = client.CreateTestUser()
+	group3, err := client.CreateGroup(context.TODO(), &monify.CreateGroupRequest{Name: "test3"})
+	assert.NoError(t, err)
+	group4, err := client.CreateGroup(context.TODO(), &monify.CreateGroupRequest{Name: "test4"})
+	assert.NoError(t, err)
+	createdGroups["test3"] = group3.GroupId
+	createdGroups["test4"] = group4.GroupId
+
 	groups, err := client.ListJoinedGroups(context.TODO(), &monify.Empty{})
 	assert.NoError(t, err)
-	assert.Equal(t, groups.GetGroups()[0].GetGroupId(), group1.GroupId)
-	assert.Equal(t, groups.GetGroups()[0].GetName(), "test1")
-	assert.Equal(t, groups.GetGroups()[1].GetGroupId(), group2.GroupId)
-	assert.Equal(t, groups.GetGroups()[1].GetName(), "test2")
+	assert.Equal(t, 2, len(groups.GetGroups()))
+	for _, group := range groups.GetGroups() {
+		assert.Equal(t, createdGroups[group.GetName()], group.GetGroupId())
+	}
+
+	client.SetTestUser(user1)
+	groups, err = client.ListJoinedGroups(context.TODO(), &monify.Empty{})
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(groups.GetGroups()))
+	for _, group := range groups.GetGroups() {
+		assert.Equal(t, createdGroups[group.GetName()], group.GetGroupId())
+	}
 }
