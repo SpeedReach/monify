eacd3cd7318d0e05232c664c201a4489b52aa743
Danielllllllllllllll <daniel0702chien@gmail.com>
2024-06-28 05:45:57 +0800

    feature: accept friend invitation sccessfully

--- services/api/test/friend_invite_test.go
+++ services/api/test/friend_invite_test.go
@@ -16,7 +16,7 @@ func TestInviteFriend(t *testing.T) {
 	assert.NoError(t, err)
 	_, err = client.InviteFriend(context.TODO(), &monify.InviteFriendRequest{ReceiverNickId: "test_nickId1"})
 	assert.Error(t, err) // cannot send invitation to yourself
-	_ = client.CreateTestUser()
+	user2 := client.CreateTestUser()
 	_, err = client.InviteFriend(context.TODO(), &monify.InviteFriendRequest{ReceiverNickId: "test_nickId1"})
 	assert.NoError(t, err)
 	_ = client.CreateTestUser()
@@ -28,4 +28,12 @@ func TestInviteFriend(t *testing.T) {
 	invitaions, err := client.ListFriendInvitation(context.TODO(), &monify.FriendEmpty{})
 	assert.NoError(t, err)
 	assert.Equal(t, len(invitaions.GetInvitation()), 2)
+
+	// test accept invitation
+	var inviteId []string
+	for i := 0; i < len(invitaions.GetInvitation()); i++ {
+		inviteId = append(inviteId, (invitaions.Invitation[i].InviteId))
+	}
+	_, err = client.AcceptInvitation(context.TODO(), &monify.AcceptInvitationRequest{User1Id: user1, User2Id: user2, InviteId: inviteId[0]})
+	assert.NoError(t, err)
 }
